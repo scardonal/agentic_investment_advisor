c94ae84a9bdaa12ebdae64588168fec396cf1cd3
Sebastian Cardona <sebastian.cardonal@perficient.com>
2025-10-31 14:11:36 -0500

    Create module for llm definition

---
+++ src/agentic_investment_advisor/models.py
@@ -0,0 +1,36 @@
+"""
+Module for defining models used in the agentic investment advisor application.
+"""
+
+from crewai import LLM
+from dotenv import load_dotenv
+from portkey_ai import createHeaders
+import os
+
+load_dotenv()
+
+PORTKEY_URL = os.getenv("PORTKEY_URL", "")
+PORTKEY_API_KEY = os.getenv("PORTKEY_API_KEY", "")
+
+manager_llm = LLM(
+    model="gemini-2.5-pro",
+    temperature=0,
+    base_url=PORTKEY_URL,
+    api_key="dummy_key",
+    extra_headers=createHeaders(
+        api_key=PORTKEY_API_KEY,
+        provider="@dsvertex",
+    ),
+)
+
+agent_llm = LLM(
+    model="us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+    temperature=0.1,
+    max_tokens=1024,
+    base_url=PORTKEY_URL,
+    api_key="dummy_key",
+    extra_headers=createHeaders(
+        api_key=PORTKEY_API_KEY,
+        provider="@aws-bedrock-use2",
+    ),
+)
